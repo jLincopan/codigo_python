5a9552df495239b9e6a167980d89cd65ab81b890
Javier Lincopán <cocolilo148@gmail.com>
2021-02-26 16:28:48 -0300

    añadidos comentarios y el bucle principal

--- culebra.py
+++ culebra.py
@@ -1,11 +1,24 @@
+from time import sleep
+
+# variables globales (por mientras, cuando esté más completo el programa
+# pienso quitarlas para solo usar variables locales)
+
 ancho = 20
 altura = 10
 
 cuerpo_culebra = "[=]"
 cabeza_culebra = "[>]"
 espacio_vacio = "[ ]"
+
+posicion_cabeza_x = 3
+posicion_cabeza_y = 0
+    
+posicion_cola_x = 0
+posicion_cola_y = 0
+
 PANTALLA = []
 
+#crea, llena y devuelve una matriz con los carácteres especificados en "caracter_inicial"
 def crearMatriz(caracter_inicial, ancho, altura):
     matriz = []
     for i in range(altura):
@@ -15,6 +28,7 @@ def crearMatriz(caracter_inicial, ancho, altura):
         matriz.append(columna)
     return matriz
 
+#llena la matriz que contiene la pantalla
 def inicializarPantalla(ancho, altura):
     global PANTALLA
     PANTALLA = crearMatriz(espacio_vacio, ancho, altura)
@@ -25,14 +39,12 @@ def mostrarPantalla(PANTALLA):
             print(PANTALLA[i][j], end="")
         print("")
 
-#TODO:Hacer que la serpiente aparezca en un lugar aleatorio
-def crearCulebra(largo):
+# Llena las posiciones que debería ocupar la serpiente en la matriz
+# que representa la pantalla, NO muestra la pantalla, solo cambia los
+# carácteres para representar la serpiente
+# TODO:Hacer que la serpiente aparezca en un lugar aleatorio
+def dibujarCulebra(largo):
     global PANTALLA
-    posicion_cabeza_x = 3
-    posicion_cabeza_y = 0
-    
-    posicion_cola_x = 0
-    posicion_cola_y = 0
 
     if posicion_cabeza_y == posicion_cola_y:
         for i in range(largo-1):
@@ -45,14 +57,28 @@ def crearCulebra(largo):
         PANTALLA[posicion_cabeza_y][posicion_cabeza_x] = cabeza_culebra
 
 def avanzar(largo):
+
+    global posicion_cabeza_x
+    global posicion_cabeza_y
+    
+    global posicion_cola_x
+    global posicion_cola_y
+
     global PANTALLA
-    for i in range(largo):
-        PANTALLA[0][i] = "[*]"
+    posicion_cabeza_x+=1
+    posicion_cola_x+=1
+    dibujarCulebra(4)
 
 
 def culebron():
     inicializarPantalla(ancho, altura)
-    crearCulebra(4)
-    mostrarPantalla(PANTALLA)
 
+    #bucle principal
+    while 1:
+        print("\033[H\033[J") 
+        mostrarPantalla(PANTALLA)
+        print()
+        avanzar(4)
+        sleep(0.250)
+    
 culebron()
